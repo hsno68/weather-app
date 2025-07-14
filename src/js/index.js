9bf604ee1135057271e5a9ef67c0854e09ad73e4
haiseno <haise.zhao@gmail.com>
2025-07-14 10:00:25 -0700

    Add function to fetch data from API

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
 import "../css/styles.css";
+import { API_KEY } from "./apiKey.js";
+
+async function fetchWeather(location = "Los Angeles") {
+  try {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`, { mode: "cors" });
+
+    if (!response.ok) {
+      throw new Error(`HTTP ${response.status}: ${response.statusText}`);
+    }
+
+    const weatherData = await response.json();
+    return weatherData;
+  } catch (error) {
+    console.error("Error in fetchWeather():", error);
+    throw error;
+  }
+}
